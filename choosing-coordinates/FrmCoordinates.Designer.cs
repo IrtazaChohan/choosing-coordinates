ea5952abd58a10298648c473739df560f933eb29
Irtaza Chohan <irtazachohan@gmail.com>
2021-09-29 12:04:01 +0100

    Add project files.

---
+++ choosing-coordinates/FrmCoordinates.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace choosing_coordinates
+{
+    partial class FrmCoordinates
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextBox1 = new System.Windows.Forms.TextBox();
+            this.txtTextBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTextBox1
+            // 
+            this.txtTextBox1.Location = new System.Drawing.Point(93, 93);
+            this.txtTextBox1.Name = "txtTextBox1";
+            this.txtTextBox1.Size = new System.Drawing.Size(81, 23);
+            this.txtTextBox1.TabIndex = 0;
+            // 
+            // txtTextBox2
+            // 
+            this.txtTextBox2.Location = new System.Drawing.Point(246, 93);
+            this.txtTextBox2.Name = "txtTextBox2";
+            this.txtTextBox2.Size = new System.Drawing.Size(81, 23);
+            this.txtTextBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(82, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quadrant Coordinate Calculator";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(163, 169);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(87, 26);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X Coordinates";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y Coordinates";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(93, 221);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Visible = false;
+            // 
+            // FrmCoordinates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 324);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTextBox2);
+            this.Controls.Add(this.txtTextBox1);
+            this.Name = "FrmCoordinates";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextBox1;
+        private System.Windows.Forms.TextBox txtTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ choosing-coordinates/FrmCoordinates.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace choosing_coordinates
+{
+    public partial class FrmCoordinates : Form
+    {
+        public FrmCoordinates()
+        {
+            InitializeComponent();
+        }
+
+        //Write a C# Sharp program to accept a coordinate point in an XY coordinate
+        //system and determine in which quadrant the coordinate point lies
+        //Test Data :
+        //Input the value for X coordinate :7
+        //Input the value for Y coordinate :9
+        //Expected Output :
+        //The coordinate point(7,9) lies in the First quadrant.
+
+        int xcoordinate = 0;
+        int ycoordinate = 0;
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            xcoordinate = int.Parse(txtTextBox1.Text);
+            ycoordinate = int.Parse(txtTextBox2.Text);
+
+            if ((xcoordinate >= 1 && xcoordinate <= 3) && (ycoordinate >= 1 && ycoordinate <= 3))
+            {
+                lblResult.Text = "You chose " + xcoordinate + "," + ycoordinate + " which is the first quadrant";
+                lblResult.Visible = true;
+            }
+            else if ((xcoordinate >= 4 && xcoordinate <= 6) && (ycoordinate >= 4 && ycoordinate <= 6))
+            {
+                lblResult.Text = "You chose " + xcoordinate + "," + ycoordinate + " which is the second quadrant";
+                lblResult.Visible = true;
+            }
+            else if ((xcoordinate >= 7 && xcoordinate <= 9) && (ycoordinate >= 7 && ycoordinate <= 9))
+            {
+                lblResult.Text = "You chose " + xcoordinate + "," + ycoordinate + " which is the third quadrant";
+                lblResult.Visible = true;
+            }
+            else if ((xcoordinate >= 10 && xcoordinate <= 12) && (ycoordinate >= 10 && ycoordinate <= 12))
+            {
+                lblResult.Text = "You chose " + xcoordinate + "," + ycoordinate + " which is the forth quadrant";
+                lblResult.Visible = true;
+            }
+            else
+            {
+                lblResult.Text = "You chose " + xcoordinate + "," + ycoordinate + " which is not in any quadrant";
+                lblResult.Visible = true;
+            }
+        }
+    }
+}
